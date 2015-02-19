0a65ddd0924cf10adc8a0edbd1f8cb7d2cab3fa3
Benjamin Steinwender <b@stbe.at>
2015-02-19 20:36:14 +0100

    added command line interface
    width and height can be specified (defaults to 1920x1080)
    number of files can be specified (defaults to 600)
    verbose mode implemented

--- randImg.py
+++ randImg.py
@@ -1,22 +1,56 @@
 #!/usr/bin/env python
 
+import sys
+import argparse
 import pypng.code.png as png
 import numpy.random as nprnd
 
-width = 1920
-height = 1080
-numFiles = 500
+#defaults:
+defwidth = 1920
+defheight = 1080
+defnumFiles = 600
 
-for n in range(numFiles):
-	f = open("b{:03d}.png".format(n), 'wb')
-	w = png.Writer(width, height)
+def arguments():
+	"""
+	parsing command line arguments
+	"""
+	parser = argparse.ArgumentParser(description="Generate random PNG images. Each pixel will be a random combination of either 0% or 100% red, green and blue.")
+	parser.add_argument("-d", "--dim", type=int, nargs=2, dest="dim", default=[defwidth, defheight], help="Dimension (width, heigth) of images")
+	parser.add_argument("-n", "--numFiles", type=int, dest="numFiles", default=defnumFiles, help="Number of images")
+	parser.add_argument("-v", "--verbose", action="store_true", dest="verbose", default=False, help="Print debug info.")
+	parser.add_argument("-s", "--stats", action="store_true", dest="stats", default=False, help="Compute statistics about color distribution (not yet implemented)")
+	return parser.parse_args()
 
-	dat = []
 
-	for i in range(height):
-		dat.append(list(nprnd.randint(2, size=3*width) * 255))
+def main(args):
+	"""
+	create n random png-images wxh pixels
+	"""
+	numdigits = len(str(args.numFiles))
+	strformat = "b{:0"+str(numdigits)+"d}.png"
+	width = args.dim[0]
+	height = args.dim[1]
 
-	w.write(f, dat)
-	f.close()
+	for n in range(args.numFiles):
+		fname = strformat.format(n)
+		if args.verbose:
+			print(fname)
+		f = open(fname, 'wb')
+		w = png.Writer(width, height)
 
-print("done")
+		dat = []
+
+		for i in range(height):
+			dat.append(list(nprnd.randint(2, size = 3 * width) * 255))
+
+		w.write(f, dat)
+		f.close()
+
+	if args.verbose:
+		print("done")
+
+
+if __name__ == '__main__':
+	args = arguments()
+	main(args)
+
